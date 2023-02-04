af944276692ac1fae4e9b6611982c9c28a5a2d2a
shimopino <shimoke4869@gmail.com>
2023-02-04 13:36:16 +0900

    テストに関する実験を追加

---
+++ tests/src/lib.rs
@@ -0,0 +1,21 @@
+mod module;
+mod unused;
+
+pub struct Fern {
+    pub size: f64,
+    pub growth_rate: f64,
+}
+
+impl Fern {
+    /// シダ植物の1日の成長をシミュレートする
+    pub fn grow(&mut self) {
+        self.size *= 1.0 + self.growth_rate;
+    }
+}
+
+/// 指定した日数でシミュレーションを実行する
+pub fn run_simulation(fern: &mut Fern, days: usize) {
+    for _ in 0..days {
+        fern.grow();
+    }
+}
---
+++ tests/src/module.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    fn roughly_equal(a: f64, b: f64) -> bool {
+        (a - b).abs() < 1e-6
+    }
+
+    #[test]
+    fn trig_works() {
+        use std::f64::consts::PI;
+        assert!(roughly_equal(PI.sin(), 0.0));
+    }
+}
