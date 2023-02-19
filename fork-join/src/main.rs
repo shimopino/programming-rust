d4b739d330f79f26c3fc5d49ea24772be7e84f2e
shimopino <shimoke4869@gmail.com>
2023-02-19 14:34:37 +0900

    Arcによるスレッド間の不変データ共通を追加

--- fork-join/src/main.rs
+++ fork-join/src/main.rs
@@ -1,9 +1,18 @@
 use std::{
-    io,
-    thread::{self, JoinHandle},
+    sync::Arc,
+    thread::{self},
+    vec,
 };
 
 fn main() {
+    process_child_thread();
+
+    let words = vec!["hello".to_string(), "world".to_string()];
+    let glossary = vec!["glossary".to_string()];
+    process_child_thread_with_immutable_data(words, Arc::new(glossary));
+}
+
+fn process_child_thread() {
     let mut thread_handles = vec![];
     for _ in 0..10 {
         thread_handles.push(thread::spawn(|| {
@@ -16,8 +25,19 @@ fn main() {
     }
 }
 
-// fn process_files_in_parallel(filenames: Vec<String>) -> io::Result<()> {
-//     const NTHREADS: usize = 8;
+fn process_child_thread_with_immutable_data(words: Vec<String>, glossary: Arc<Vec<String>>) {
+    let mut thread_handles = vec![];
+    for word in words {
+        let glossary_for_child = glossary.clone();
+        thread_handles.push(thread::spawn(move || {
+            println!(
+                "hello from a child thread: {}, {:?}",
+                word, &glossary_for_child
+            );
+        }))
+    }
 
-//     let worklists = aplit_vec_into_chunk(filenames, NTHREADS);
-// }
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
+}
